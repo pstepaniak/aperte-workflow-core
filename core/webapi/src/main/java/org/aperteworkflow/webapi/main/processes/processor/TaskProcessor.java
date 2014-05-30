a7d68be3e9c2b2b1d65370019f555cc6120af6ee
Rafał Surowiecki <rsurowiecki@bluesoft.net.pl>
2014-05-30 10:32:47 +0200

    Audit log

--- core/webapi/src/main/java/org/aperteworkflow/webapi/main/processes/processor/TaskProcessor.java
+++ core/webapi/src/main/java/org/aperteworkflow/webapi/main/processes/processor/TaskProcessor.java
@@ -1,12 +1,13 @@
 package org.aperteworkflow.webapi.main.processes.processor;
 
 import org.aperteworkflow.webapi.main.processes.domain.HtmlWidget;
-import pl.net.bluesoft.rnd.processtool.model.BpmTask;
-import pl.net.bluesoft.rnd.processtool.model.IAttributesConsumer;
-import pl.net.bluesoft.rnd.processtool.model.IAttributesProvider;
+import pl.net.bluesoft.rnd.processtool.model.*;
+import pl.net.bluesoft.rnd.processtool.ui.widgets.HandlingResult;
 import pl.net.bluesoft.rnd.util.i18n.I18NSource;
 
 import java.util.Collection;
+import java.util.Date;
+import java.util.logging.Level;
 
 /**
  * Created by pkuciapski on 2014-05-15.
@@ -28,4 +29,28 @@ public class TaskProcessor extends AbstractSaveProcessor {
     protected IAttributesConsumer getConsumer() {
         return task.getProcessInstance();
     }
+
+	protected void auditLog(Collection<HandlingResult> results) {
+		ProcessInstance process = getProvider().getProcessInstance();
+
+		if (process != null && !results.isEmpty()) {
+			String json = null;
+			try {
+				json = mapper.writeValueAsString(results);
+			} catch (Exception e) {
+				logger.log(Level.SEVERE, e.getMessage(), e);
+			}
+			ProcessInstanceLog log = new ProcessInstanceLog();
+			log.setState(null);
+			log.setEntryDate(new Date());
+			log.setEventI18NKey("process.log.process-change");
+			// todo
+			if (getProvider() instanceof BpmTask)
+				log.setUserLogin(((BpmTask) getProvider()).getAssignee());
+			log.setLogType(ProcessInstanceLog.LOG_TYPE_PROCESS_CHANGE);
+			log.setOwnProcessInstance(process);
+			log.setLogValue(json);
+			process.getRootProcessInstance().addProcessLog(log);
+		}
+	}
 }
